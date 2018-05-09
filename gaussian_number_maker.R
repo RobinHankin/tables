f1b902aaa6637a258678d1149eba33654ff5c5fb
RobinHankin <hankin.robin@gmail.com>
2018-05-09 15:15:33 +1200

    gaussian_number_maker.R

--- gaussian_number_maker.R
+++ gaussian_number_maker.R
@@ -1,6 +1,7 @@
+filename <- "gaussian_table_values.txt"
 library("magrittr")
 x <- 
-  seq(from=0, by=0.01,to=2.99) %>%
+  seq(from=0, by=0.01,to=3.09) %>%
   pnorm                        %>%
   round(4)                     %>%
   sprintf("%1.4f", .)          %>%
@@ -9,12 +10,27 @@ x <-
   noquote
 
 rownames(x) <-
-  seq(from=0,to=2.9,by=0.1)    %>%
+  seq(from=0,to=3.0,by=0.1)    %>%
   sprintf("%1.1f", .)       
-
 
 colnames(x) <- 
   seq(from=0,to=0.09,by=0.01)  %>%
   sprintf("%1.2f", .)         
+
+
+header <- paste(colnames(x),collapse = " & ")  %>% paste("\\\\  \\hline",collapse="")
+header <- paste("&" ,header)
+write(header,file=filename,append=FALSE)
+
+for(i in seq_len(nrow(x))){
+  bodyline <- paste(rownames(x)[i],paste(x[i,],collapse = "&"),sep= "& \\,\\, ")
+  print(bodyline)
+  if((i>1) & (i%%5==1)){
+    bodyline %<>% paste("\\\\[3mm]")
+  } else {
+    bodyline %<>% paste("\\\\")
+  }
+  write(bodyline,file=filename,append=TRUE)
+}
 
 

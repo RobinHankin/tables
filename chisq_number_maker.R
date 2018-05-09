4c5ee817b7c740784a10a6d86b2a672552e0817b
RobinHankin <hankin.robin@gmail.com>
2018-05-09 13:10:22 +1200

    slight rationalization

--- chisq_number_maker.R
+++ chisq_number_maker.R
@@ -1,6 +1,9 @@
 library("magrittr")
 
-expand.grid(1:30,c(0.9,0.95,0.975,0.99,0.995)) %>% 
-apply(1,function(x){qchisq(x[2],df=x[1])})     %>%
-matrix(ncol=5)                                 %>%
+p <- c(0.9,0.95,0.975,0.99,0.995)
+df <- 1:30
+
+expand.grid(df,p)                          %>% 
+apply(1,function(x){qchisq(x[2],df=x[1])}) %>%
+matrix(ncol=length(df))                    %>%
 round(2)

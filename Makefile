947c05b474ec65dca48dc569d18667a09c47e070
RobinHankin <hankin.robin@gmail.com>
2018-05-22 11:22:36 +1200

    start improving the chi-square stuff

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ all:
 	R CMD BATCH gaussian_diagram.R
 	R CMD BATCH gaussian_number_maker.R
 	pdflatex table_gaussian_distribution_function
+	R CMD BATCH chisq_diagram.R
+	R CMD BATCH chisq_number_maker.R
 
 clean:
 	rm -f  *.aux *.log *.out *.Rout *.txt gaussian_diagram.pdf table_gaussian_distribution_function.pdf
--- chisq_number_maker.R
+++ chisq_number_maker.R
@@ -1,9 +1,48 @@
+## This file creates "chisq_table_values.txt" which holds suitably
+## rounded numerical values of critical points of the chisquare
+## distribution.  File chisq_table_values.txt is imported by file
+## table_chisq.tex
+
+
 library("magrittr")
+filename <- "chisq_table_values.txt"
 
 p <- c(0.9,0.95,0.975,0.99,0.995)
 df <- 1:30
 
-expand.grid(df,p)                          %>% 
-apply(1,function(x){qchisq(x[2],df=x[1])}) %>%
-matrix(ncol=length(df))                    %>%
-round(2)
+x  <-
+  expand.grid(p,df)                          %>% 
+  apply(1,function(x){qchisq(x[1],df=x[2])}) %>%
+  matrix(ncol=length(p),byrow=TRUE)  %>%
+  round(2)
+
+
+x[x>10] %<>% round(1) # only one decimal place for large values
+
+
+
+
+
+rownames(x) <- as.character(df)
+colnames(x) <-  p
+
+
+
+write("",file=filename,append=FALSE)
+
+for(i in seq_len(nrow(x))){
+  bodyline <- paste("{\\large ",rownames(x)[i],"}&",collapse="")  %>%
+    paste(paste(x[i,],collapse="&"))
+  if((i>1) & (i%%5==1)){
+    bodyline %<>% paste("\\\\[3mm]")
+  } else {
+    bodyline %<>% paste("\\\\")
+  }
+  write(bodyline,file=filename,append=TRUE)
+}
+
+
+
+
+
+  stop()

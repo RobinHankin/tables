480023a865bbcd6e213ab0013154654acd6ec3fd
RobinHankin <hankin.robin@gmail.com>
2018-05-28 14:42:11 +1200

    rationalize the Gaussian diagrams

--- gaussian_diagram.R
+++ gaussian_diagram.R
@@ -1,15 +1,56 @@
-pdf(file="gaussian_diagram.pdf",width=5,height=3)
 n <- 100 # smoothness
-x <- seq(from=-3,to=3,len=n)
-plot(x,dnorm(x),axes=FALSE,xlab='',ylab='',lwd=3,type='n',asp=4,xlim=c(-3,3))
 
-x <- seq(from=-3,to=1,len=n)
-polygon(x=c(x,rev(x)),y=c(rep(0,n),dnorm(rev(x))),col='gray',border=NA)
 
-x <- seq(from=-3,to=3,len=n)
-points(x,dnorm(x),type='l',lwd=3)
-abline(0,0)
-par(xpd=TRUE)
-text(1,-0.1,'z')
+setup <- function(n=100){
+  x <- seq(from=-3,to=3,len=n)
+  plot(x,dnorm(x),axes=FALSE,xlab='',ylab='',lwd=3,type='n',asp=4,xlim=c(-3,3))
+}
+  
+outline <- function(n=100){
+  x <- seq(from=-3,to=3,len=n)
+  points(x,dnorm(x),type='l',lwd=3)
+  abline(0,0)
+}
 
+shading <- function(a,b){
+  x <- seq(from=a,to=b,len=n)
+  polygon(x=c(x,rev(x)),y=c(rep(0,n),dnorm(rev(x))),col='gray',border=NA)
+}
+
+pdf(file="gaussian_diagram1.pdf",width=5,height=3)
+{
+  setup()
+  shading(-3,1)
+  outline()
+  par(xpd=TRUE)
+  text(1,-0.1,'z')
+}
 dev.off()
+
+pdf(file="gaussian_diagram2.pdf",width=5,height=3)
+{
+  setup()
+  shading(-3,-1)
+  outline()
+  par(xpd=TRUE)
+  text(-1,-0.1,'z')
+}
+dev.off()
+
+pdf(file="gaussian_diagram3.pdf",width=5,height=3)
+{
+  setup()
+  shading(-3,-1)
+  shading(+1,+3)
+  outline()
+  par(xpd=TRUE)
+  text(-1,-0.1,'K')
+  text(+1,-0.1,'K')
+}
+dev.off()
+
+
+
+
+
+

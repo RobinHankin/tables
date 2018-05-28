034d274d5b518e1525d3a6288416bfe59e1bf365
RobinHankin <hankin.robin@gmail.com>
2018-05-29 09:56:35 +1200

    add bespokes student diagram maker

--- gaussian_diagram.R
+++ gaussian_diagram.R
@@ -12,9 +12,9 @@ outline <- function(n=100){
   abline(0,0)
 }
 
-shading <- function(a,b){
+shading <- function(a,b,f=dnorm){
   x <- seq(from=a,to=b,len=n)
-  polygon(x=c(x,rev(x)),y=c(rep(0,n),dnorm(rev(x))),col='gray',border=NA)
+  polygon(x=c(x,rev(x)),y=c(rep(0,n),f(rev(x))),col='gray',border=NA)
 }
 
 pdf(file="gaussian_diagram1.pdf",width=5,height=3)
@@ -49,8 +49,15 @@ pdf(file="gaussian_diagram3.pdf",width=5,height=3)
 }
 dev.off()
 
-
-
-
-
-
+pdf(file="student_diagram.pdf",width=5,height=3)
+{
+  x <- seq(from=-2,to=2,len=n)
+  plot(x,dnorm(x),axes=FALSE,xlab='',ylab='',lwd=3,type='n',xlim=c(-2,2),ylim=c(0,0.35))
+  x <- seq(from=-2,to=2,len=n)
+  shading(-2,+1,f=function(x){dt(x,df=1)})
+  points(x,dt(x,df=1),type='l',lwd=3)
+  abline(0,0)
+  par(xpd=TRUE)
+  text(+1,-0.1,'z')
+}
+dev.off()

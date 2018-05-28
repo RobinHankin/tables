6fb90c9d67712d7114168fadd285a577886bd6f9
RobinHankin <hankin.robin@gmail.com>
2018-05-28 15:03:34 +1200

    this is needed for table_gaussian_percentiles.tex

---
+++ gaussian_number_maker2.R
@@ -0,0 +1,21 @@
+## This file creates "gaussian_table_values2.txt" which holds suitably
+## rounded numerical values of Gaussian distribution function in a
+## nice LaTeX format including nice line spacing.  File
+## gaussian_table_values.txt is imported by file
+## table_gaussian_percentile.tex.
+
+filename <- "gaussian_table_values2.txt"
+
+qq <- c(0.001,0.005,seq(from=0.01,to=0.04,by=0.01),seq(from=0.05,to=0.2,by=0.05),0.3,0.4)
+qq <- sort(c(qq,0.5,1-qq))   # increasing
+
+library("magrittr")
+
+write("",file=filename,append=FALSE)
+
+for(i in qq){
+  bodyline <- paste(i,round(qnorm(i),4), sep=" & ") %>% paste("\\\\")
+  write(bodyline,file=filename,append=TRUE)
+}
+
+

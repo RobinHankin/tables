2756f76ba7633da10a665b027c5462a45c344238
RobinHankin <hankin.robin@gmail.com>
2018-06-04 11:10:31 +1200

    add this

---
+++ antilog_number_maker.R
@@ -0,0 +1,48 @@
+## This file creates "antilog_table_values.txt" which holds suitably
+## rounded (integer!) values for the log table.
+## File log_table_values.txt is imported by table_log.tex.
+rm(list=ls())
+library("magrittr")
+
+
+quick <- TRUE # change this for faster working when debugging
+if(quick){
+    load("antilog.Rdata")
+} else {
+    source("antilogtable.R")   # creates R variables table_main and table_Delta
+}
+
+formatted_main <- function(x){
+    out <- as.character(x)
+    isn <- is.na(x)
+    if(any(!isn)){ out[!isn] <- sprintf("%04.0f",x[!isn])}
+    if(any( isn)){ out[ isn] <- " "}
+    return(out)
+}
+
+formatted_Delta <- function(x){sprintf("%04.0f",x)}
+
+table_filewriter <- function(filename,main,Delta,fun){
+
+    write("%this file is not human-readable, it was created by 'antilog_number_maker.R'",
+          file=filename, append=FALSE)
+    for(i in seq_len(nrow(main))){
+    
+        text_rows <- c(formatted_main(main[i,]),Delta[i,])
+        bodyline <- paste("{\\large ",rownames(main)[i],"}&",collapse="")  %>%
+            paste(paste(text_rows,collapse="&"))
+        if(fun(i)){
+            bodyline %<>% paste("\\\\[3mm]")
+        } else {
+            bodyline %<>% paste("\\\\")
+        }
+        write(bodyline,file=filename,append=TRUE)
+    }
+}
+
+## Full table first:
+first_page <- 1:50
+linespacefunction <- function(i){ i%%5==0 }
+
+table_filewriter("antilog_table_values_page1.txt",antilogtable_main[ first_page,],antilogtable_delta[ first_page,],fun=linespacefunction)
+table_filewriter("antilog_table_values_page2.txt",antilogtable_main[-first_page,],antilogtable_delta[-first_page,],fun=linespacefunction)

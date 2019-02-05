b727280804e7cce9ae030d7a286246562dd12f86
RobinHankin <hankin.robin@gmail.com>
2019-02-06 11:56:09 +1300

    better inline documentation

--- logtable.R
+++ logtable.R
@@ -1,5 +1,7 @@
 ## This file is designed to be called by "log_number_maker.R".
 
+## It takes about 10 seconds to run.
+
 ## Makes R variables 'table_main' and 'table_Delta' (both matrices),
 ## which are the main and Delta parts of the log table.  Also makes R
 ## variables 'simple_main' and 'simple_Delta' which are the same but
@@ -24,8 +26,9 @@ log <- function(...){stop("do not use log(), use log10() here")}
 func <- log10
 
 tableentry <- function(x,numerical=TRUE){
- ## try x=1.32, table entry is "1206" (that is, the actual table
- ## entry, as it actually appears on the table; notionally an integer)
+ ## Example: x=1.32, we have log10(x)=0.1205739 from R; table entry is
+ ## "1206" (that is, the actual table entry, as it actually appears on
+ ## the table; notionally an integer)
 
   out <- round(func(x)*10000)
   if(numerical){
@@ -36,58 +39,106 @@ tableentry <- function(x,numerical=TRUE){
 }
 
 tablevalue <- function(x){
-  ## try x=1.31, table gives .1173 [that is, the numerical equivalent
-  ## of the table entry as given by tableentry()
+  ## Example: x=1.31, we have log10(x)=0.1172713 from R; table entry
+  ## of "1173" would be interpreted as 0.1173 [that is, the numerical
+  ## equivalent of the table entry as given by tableentry()]
 
   tableentry(x)/10000
 }
 
-
 tablevalue_delta <- function(x, Delta){
-  ## try x=1.31, delta=3, table gives 1173+3=1176 -> 0.1176 returned
+  ## This function evaluates the effect of a suggested Delta value in
+  ## the differences part of the table.  The idea is that we will try
+  ## different values of Delta and see which value is the "best", as
+  ## measured by badness().  For example, tablevalue_delta(x=1.31,
+  ## Delta=8) returns the table's value at the point x=1.31, if the
+  ## value of Delta used is 8.  The table gives "1173" for x=1.31, and
+  ## if Delta=8 this is 1173+8=1181, which would mean that 0.1181 is
+  ## given by the table.
 
-  tablevalue(x) + Delta/10000  
+  tablevalue(x) + Delta/10000
 }
 
-tableerror <- function(x, third_digit, Delta){  
-  ## for 1.326, use x=1.32, third_digit=6.  Function returns the
-  ## *numerical* difference between the true value and the value given
-  ## by the table.
+tableerror <- function(x, third_digit, Delta){
+    ## This function calculates the error induced by using the table for
+    ## a particular value of x and third_digit, when using a particular
+    ## value of Delta.  We will try different values of Delta and see
+    ## which one is the "best".
+
+    ## For example, consider
+
+    ## tableerror(x=1.32, third_digit=4, Delta=19)
+
+    ## This means we are considering the log10 of 1.324=0.121888.  The
+    ## table would give "1206" with a delta of 19 which would
+    ## correspond to 1206+19=1225, with a numerical value of 0.1225
+
+    ## So the difference between the true value and the value given by
+    ## the table would be 0.121888-0.1225= -0.000612
 
   true_value <- func(x+third_digit/1000)
-  table_value <- tablevalue_delta(x,Delta=Delta)
-  return(table_value-true_value)
+    table_value <- tablevalue_delta(x,Delta=Delta)
+    return(table_value-true_value)
 }
 
 error <- function(x,third_digit,Delta){
-  ## Returns the error from each of a series of x values.  Try
-  ## error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=16) and
-  ## compare this with
-  ## error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=17) to
-  ## see whether Delta=16 is better or worse than Delta=17 for the "5"
-  ## entry of the differences on the "1.3" row of the table.  See how
-  ## a *single* Delta value (here we are choosing between 16 and 17)
-  ## gives rise to 10 different errors, one for each of 1.30 -> 1.305,
-  ## 1.31 -> 1.315, 1.32 -> 1.325,..., 1.39 -> 1.395.
+
+    ## Returns the error from each of a series of x values: given a
+    ## particular value of Delta, error() returns the difference between
+    ## the true value of log(x) and the value given by the table.
+
+    ## For example, consider:
+
+    ## error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=16)
+
+    ## and compare this with
+
+    ## error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=17)
+
+    ## so we can see whether Delta=16 is better or worse than Delta=17
+    ## for the "5" entry of the differences on the "1.3" row of the
+    ## table.
+
+    ##  error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=16)
+    ## [1] -1.1051e-04 -2.5752e-05 -1.5878e-05  1.8734e-05 -2.2284e-05
+    ## >  error(x=seq(from=1.3,by=0.01,to=1.34),third_digit=5,Delta=17)
+    ## [1] -1.0511e-05  7.4247e-05  8.4121e-05  1.1873e-04  7.7715e-05
+
+    ## (above cut-n-pasted but slightly edited for clarity).  More
+    ## explicitly, we would have, for Delta=16, the following:
+
+    ## 1.30 -> 1.305: 1139+16=1155; 0.1155-log10(1.305) = -0.0001105117
+    ## 1.31 -> 1.315: 1173+16=1189; 0.1189-log10(1.315) = -0.0000257528
+    ## 1.32 -> 1.325: 1206+16=1222; 0.1222-log10(1.325) = -0.0000158782
+    ## 1.33 -> 1.335: 1239+16=1255; 0.1255-log10(1.335) = +0.0001873430
+    ## 1.34 -> 1.345: 1271+16=1287; 0.1287-log10(1.345) = -0.0000228434
+
+    ## See how a *single* Delta value (here we are using 16) gives
+    ## rise to 5 different errors when using a third digit of 5, one
+    ## for each the five cases it would be used.  In this case have
+    ## four negative errors and one positive.  From the R session
+    ## above, with Delta=17 we have one negative and four positive
+    ## errors.
 
   sapply(x,function(x){tableerror(x, third_digit=third_digit, Delta=Delta)})
 }
 
 badness <- function(x,third_digit,Delta,measure){
-  
+
   ## As per the comments in error(), any Delta value [we were
   ## comparing Delta=16 and Delta=17 above, for the '5' entry on the
-  ## '1.3' row] has associated with it 10 distinct errors, one for
-  ## each column of its row.  To choose a particular value of Delta,
-  ## here to choose whether 16 is preferable to 17, we need to
-  ## summarize the 10 error values.  We can do this either by
-  ## returning the maximum absolute error ('max'), the root mean
-  ## square error ('mse'), or the mean absolute deviation ('mad').
-  ## Function badness() returns either the max, mse, or mad as
-  ## required.  Note that these three different summary methods give
-  ## different measures of badness, and this means that the value of
-  ## Delta might differ between max,mse, and mad.
-  
+  ## '1.3' row] has associated with it 5 or 10 distinct errors, one
+  ## for each column of its row.  To choose a particular value of
+  ## Delta, for example to choose whether 16 is preferable to 17, we
+  ## need to summarize *all* the error values associated with the
+  ## different values of Delta.  We can do this either by returning
+  ## the maximum absolute error ('max'), the root mean square error
+  ## ('mse'), or the mean absolute deviation ('mad').  Function
+  ## badness() returns either the max, mse, or mad as required.  Note
+  ## that these three different summary methods give different
+  ## measures of badness, and this means that the value of Delta might
+  ## differ between max,mse, and mad.
+
   error <- error(x,third_digit,Delta)
   switch(measure,
          max=max(abs(error)),      # max = Maximum error
@@ -96,7 +147,8 @@ badness <- function(x,third_digit,Delta,measure){
          )
 }
 
-differences <- function(x,show=FALSE){   
+differences <- function(x,show=FALSE){
+
   ## Given a particular value of x, which specifies a row of the
   ## table, function differences() finds the "best" value of Delta [it
   ## tries everything from Delta=0 to Delta=40] with respect to the
@@ -118,7 +170,7 @@ differences <- function(x,show=FALSE){
   ## the optimal value of Delta for third_digit = 1,2,...,9 [here,
   ## 'optimal' means 'value of Delta that mimimizes the max() of the
   ## absolute error values'].
-  
+
   out <- rbind(max,mse,mad)
   colnames(out) <- as.character(third_digit)
   out <- rbind(out,range=apply(out,2,function(x){max(x)-min(x)}))
@@ -135,7 +187,8 @@ differences <- function(x,show=FALSE){
   return(out)
 }
 
-di <- function(x,l,give=FALSE,norm_choice=1){ 
+di <- function(x,l,give=FALSE,norm_choice=1){
+
   ## Function di() is a cut-down version of differences() which
   ## returns a list of length two, the first element of which is the
   ## main table entry for x, the second is the Delta entries.
@@ -145,7 +198,7 @@ di <- function(x,l,give=FALSE,norm_choice=1){
 
   ## there is more documentation in di() of antilogtable.R, which
   ## discusses the norm_choice argument.
-  
+
   x <- seq(from=x,by=0.01,len=l)
   main_table <- tableentry(x)
   names(main_table) <- x
@@ -170,7 +223,7 @@ process_rownames <- function(x){
   odd <- round(x*100)%%10 != 0
   out[odd] <- " "
   out[!odd] <- sprintf("%1.1f",x[!odd])
-  
+
   return(out)
 }
 
@@ -178,7 +231,7 @@ if(showdebug){
   x <- seq(from=2.4,by=0.01,len=10)
   dd <- differences(x)
   print(dd)
-  
+
   print(di(1.3,5))
   print(di(3.4,10))
 }
@@ -189,7 +242,7 @@ if(showdebug){
 xsplit <- seq(from=1.0,to=1.35,by=0.05) # split rows
 xfull <- seq(from=1.4,to=9.9,by=0.1) # full rows
 
-table_main <- matrix(NA,length(xsplit),10)  
+table_main <- matrix(NA,length(xsplit),10)
 table_Delta <- matrix(NA,0,9)    # sic
 
 ## First, do the split rows:
@@ -237,5 +290,3 @@ colnames(simple_main) <- 0:9
 colnames(simple_Delta) <- 1:9
 
 save(table_main,table_Delta, simple_main,simple_Delta, file="log.Rdata")
-
-
